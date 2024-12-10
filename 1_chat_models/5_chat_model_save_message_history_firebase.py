d520dbc91031208ca1fde48d7f923b6e0c087905
nusenyaw66 <nusenyaw@gmail.com>
2024-12-11 00:17:22 +0800

    added Firestore API test func

--- 1_chat_models/5_chat_model_save_message_history_firebase.py
+++ 1_chat_models/5_chat_model_save_message_history_firebase.py
@@ -18,7 +18,7 @@ Steps to replicate this example:
         - https://cloud.google.com/docs/authentication/provide-credentials-adc#local-dev
     - Set your default project to the new Firebase project you created
 5. Enable the Firestore API in the Google Cloud Console:
-    - https://console.cloud.google.com/apis/enablcleareflow?apiid=firestore.googleapis.com&project=crewai-automation
+    - https://console.cloud.google.com/apis/enableflow?apiid=firestore.googleapis.com&project=crewai-automation
 """
 
 load_dotenv()
